a0c4fb71a3bbaedd002f62906be13e78d35e4493
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-03-06 09:19:12 +0200

    task-1done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+
+const createGalleryMarkup = (arr) => {
+    const galleryInnerMarkup = arr.map(el => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${el.original}">
+            <img
+              class="gallery__image"
+              src="${el.preview}"
+              data-source="${el.original}"
+              alt="${el.description}"
+            />
+          </a>
+        </div>`
+    }).join('');
+
+return galleryEl.insertAdjacentHTML('afterbegin', galleryInnerMarkup);
+};
+
+
+
+const handlerOnImageClick = (event) => {
+    event.preventDefault();
+
+    if(event.target.nodeName !== 'IMG') {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img class="gallery__image" src="${event.target.dataset.source}" width="800" height="600">
+    `)
+    
+    instance.show();
+    
+
+    const handlerOnEscClick = (event) => {
+        if(event.code !== 'Escape') {
+            return;
+        }
+    
+        instance.close();
+
+        window.removeEventListener('keydown', handlerOnEscClick);
+    };
+
+
+    if(instance.visible()){
+    window.addEventListener('keydown', handlerOnEscClick)  
+    }
+};
+
+
+createGalleryMarkup(galleryItems);
+
+galleryEl.addEventListener('click', handlerOnImageClick);
+
+
+
+// console.log(galleryItems);
